a56136eea780019c7c54a2cddfee8d6fc88cca27
Gururajsajjan01 <gururajsajjan03@gmail.com>
2025-03-10 15:10:27 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,35 @@
+# Integer
+a = 10
+print("Value:", a, "| Type:", type(a))
+
+# Float
+b = 3.14
+print("Value:", b, "| Type:", type(b))
+
+# String
+c = "Hello, World!"
+print("Value:", c, "| Type:", type(c))
+
+# Boolean
+d = True
+print("Value:", d, "| Type:", type(d))
+
+# List
+e = [1, 2, 3, 4, 5]
+print("Value:", e, "| Type:", type(e))
+
+# Tuple
+f = (10, 20, 30)
+print("Value:", f, "| Type:", type(f))
+
+# Set
+g = {1, 2, 3, 4}
+print("Value:", g, "| Type:", type(g))
+
+# Dictionary
+h = {"name": "Alice", "age": 25}
+print("Value:", h, "| Type:", type(h))
+
+# NoneType
+i = None
+print("Value:", i, "| Type:", type(i))
